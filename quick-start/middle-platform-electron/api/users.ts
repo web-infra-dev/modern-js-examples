a897ed20a49a4c82e6065effe8f2dc863c2514f4
pikun <1457431899@qq.com>
2021-10-26 19:10:10 +0800

    Revert "feat: electron example"

--- quick-start/middle-platform-electron/api/users.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default async (): Promise<{ key:string, name: string, age: number, country: string }[]> => {
-  const res = await axios.get(
-    'https://lf3-static.bytednsdoc.com/obj/eden-cn/beeh7uvzhq/users.json',
-  );
-  return res.data;
-};
--- quick-start/middle-platform-electron/src/modern-app-env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types='@modern-js/app-tools/types' />
-/// <reference types="@modern-js/runtime" />
-/// <reference types="@modern-js/plugin-electron/global" />
