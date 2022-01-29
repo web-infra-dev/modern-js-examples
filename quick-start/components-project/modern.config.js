e4df35d398f7c08bba16a8fb273e846c1f6b7233
Belinda Cao <caohuilin@bytedance.com>
2022-01-29 17:17:14 +0800

    fix: component project

--- quick-start/components-project/modern.config.js
+++ quick-start/components-project/modern.config.js
@@ -3,4 +3,7 @@ module.exports = {
   runtime: {
     state: true,
   },
+  testing: {
+    transformer: 'ts-jest',
+  },
 };
--- quick-start/components-project/src/index.tsx
+++ quick-start/components-project/src/index.tsx
@@ -1,2 +1,4 @@
 export { default as TableList } from './tableList';
 export { default as Avatar } from './avatar';
+
+export default () => <div>Hello Modern.js</div>;
