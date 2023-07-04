09402150fbf338d3c99f61e91616646d423ad262
chenjiahan <chenjiahan.jait@bytedance.com>
2023-07-04 15:02:23 +0800

    feat(builder-vue3): add HTML template

--- examples/builder-vue3/builder.config.ts
+++ examples/builder-vue3/builder.config.ts
@@ -4,4 +4,7 @@ import { builderPluginVue } from '@modern-js/builder-plugin-vue';
 // https://modernjs.dev/builder/en/api/index.html
 export default defineConfig({
   builderPlugins: [builderPluginVue()],
+  html: {
+    template: './src/template/index.html',
+  },
 });
