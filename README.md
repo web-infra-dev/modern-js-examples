4832a098f917b2320abed4be25568b1cb87d13b9
neverland <chenjiahan.jait@bytedance.com>
2023-07-19 11:53:35 +0800

    feat: add new builder-vue2 example (#33)

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Examples for [Modern.js](https://github.com/web-infra-dev/modern.js).
 Here are some examples you can refer to:
 
 - [builder-vue3](./examples/builder-vue3/): Basic Vue 3 App built with Modern.js Builder.
+- [builder-vue2](./examples/builder-vue2/): Basic Vue 2 App built with Modern.js Builder.
 - [runtime-api](./examples/runtime-api/): Sample code for the Modern.js Runtime API.
---
+++ examples/builder-vue2/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="root">
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    <HelloWorld msg="Modern.js + Vue" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+export default {
+  components: { HelloWorld },
+};
+</script>
+
+<style scoped lang="less">
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+
+  &:hover {
+    filter: drop-shadow(0 0 2em #eeea8caa);
+  }
+
+  &.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
+}
+</style>
