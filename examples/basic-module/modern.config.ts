ff935ca8cf31de73641707f844aa32d40e555056
h7ml <h7ml@qq.com>
2024-04-12 17:47:08 +0800

    feat: Add type definitions to defineConfig in basic-module

--- examples/basic-module/modern.config.ts
+++ examples/basic-module/modern.config.ts
@@ -1,6 +1,12 @@
-import { moduleTools, defineConfig } from '@modern-js/module-tools';
+import {
+  moduleTools,
+  defineConfig,
+  ModuleConfigParams,
+} from '@modern-js/module-tools';
+import type { UserConfigExport } from '@modern-js/core';
 
-export default defineConfig({
+const config: UserConfigExport<ModuleConfigParams> = {
   plugins: [moduleTools()],
   buildPreset: 'npm-library',
-});
+};
+export default defineConfig(config);
