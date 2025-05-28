2fd49279934e83da2ac12c6dd5eaa39c0cd95956
kongjiacong <kongjiacong@bytedance.com>
2025-05-28 14:12:03 +0800

    feat: update mf example's mf plugin version

--- examples/module-federation/base/remote/modern.config.ts
+++ examples/module-federation/base/remote/modern.config.ts
@@ -1,12 +1,14 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
 import { moduleFederationPlugin } from '@module-federation/modern-js';
 
-const isLocal = process.env.IS_LOCAL === 'true';
-
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
   server: {
     port: 3051,
+    ssr: {
+      mode: 'stream',
+      disablePrerender: true,
+    },
   },
   runtime: {
     router: true,
@@ -15,7 +17,7 @@ export default defineConfig({
     // Now this configuration is only used in the local when you run modern serve command.
     // If you want to deploy the application to the platform, use your own domain name.
     // Module federation will automatically write it to mf-manifest.json, which influences consumer to fetch remoteEntry.js.
-    assetPrefix: isLocal ? 'http://127.0.0.1:3051' : '/',
+    assetPrefix: 'http://127.0.0.1:3051',
   },
   plugins: [
     appTools({
